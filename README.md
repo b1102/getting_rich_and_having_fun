2ac740dc34426c4d6245fd7546ece481e72a4fed
sergkobiakov <sergkobiakov@gmail.com>
2021-08-16 15:27:07 +0200

    straight line approximation

--- README.md
+++ README.md
@@ -1,8 +1,15 @@
+The repo is code that is used in the https://www.facebook.com/groups/1188528704980230 facebook group.
+
+Do you know this annoying ads: I will teach you how to earn x percent per year. I have no idea how to do it. But we will find a way and have some fun) or just fun)
+
+
 # getting_rich_and_having_fun
 To create new environment execute:
+* **conda env create -f tensorflow-gpu.yml**
+* **conda activate tensorflow-gpu**
+* **python -m ipykernel install --user --name tensorflow-gpu --display-name tensorflow-gpu**
 
-**conda env create -f tensor-flow.yml**
-
-**conda activate tensor-flow**
 
 To remove the environment
+* **conda deactivate**
+* **conda remove --name tensorflow-gpu --all**
--- chapter_0/line_approximations.py
+++ chapter_0/line_approximations.py
@@ -1,50 +1,75 @@
-import matplotlib.pyplot as plt
 import numpy as np
 import tensorflow as tf
 from tensorflow import keras
 import seaborn as sns
 import pandas as pd
+import os
 from matplotlib import pyplot
+import matplotlib.pyplot as plt
+
+sns.set_style("darkgrid")
+sns.set(font_scale=2)
+
+os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
+
+# %%
 
 activation = 'relu'
 batch_size = 128
-neurons_number = 128
+neurons_number = 64
 epochs = 20
 optimize = 'Adam'
+
+# %%
 
 # build the model
 model = tf.keras.models.Sequential()
 model.add(keras.layers.Dense(neurons_number, input_shape=(1,), name='input', activation=activation))
-model.add(keras.layers.Dense(neurons_number, name='dense_layer_2', activation=activation))
+model.add(keras.layers.Dense(neurons_number, activation=activation))
 # model.add(keras.layers.Dropout(0.2))
-model.add(keras.layers.Dense(neurons_number, name='dense_layer_3', activation=activation))
+model.add(keras.layers.Dense(neurons_number, activation=activation))
 # model.add(keras.layers.Dropout(0.2))
-model.add(keras.layers.Dense(neurons_number, name='dense_layer_4', activation=activation))
+model.add(keras.layers.Dense(neurons_number, activation=activation))
 # model.add(keras.layers.Dropout(0.2))
 model.add(keras.layers.Dense(1, name='output'))
 # summary of the model
 model.summary()
 model.compile(optimizer=optimize, loss='mean_squared_error', metrics=['mse'])
 
+# %%
+
 X_train = np.linspace(0, 0.8, num=10000)
 Y_train = X_train
 
+# %%
+
 model.fit(X_train, Y_train, batch_size=batch_size, epochs=epochs, verbose=1, validation_split=0.2)
+
+# %%
 
 X_test = np.linspace(0, 0.8, num=333)
 Y_test = X_test
 
-test_loss, test_acc = model.evaluate(X_test, Y_test)
+loss, mse = model.evaluate(X_test, Y_test)
 
-to_predict = np.linspace(0, 1.0, 150)
-predict = model.predict(to_predict).reshape((150,))
-predictions = pd.DataFrame({'X_test': to_predict, 'Y_test': predict}, columns=['X_test', 'Y_test'])
+# %%
 
-training = pd.DataFrame({'X_test': X_train, 'Y_test': Y_train}, columns=['X_test', 'Y_test'])
+grid = np.linspace(0, 1.0, 150)
+predicted = model.predict(grid).reshape((150,))
+
+references = pd.DataFrame({'X': grid, 'Y_reference': grid})
+predictions = pd.DataFrame({'X': grid, 'Y_predicted': predicted})
+
+# %%
 
 dims = (30, 20)
 fig, ax = pyplot.subplots(figsize=dims)
+predicted = model.predict(grid).reshape((150,))
 
-sns.lineplot(data=predictions, x="X_test", y="Y_test", color="red")
-sns.lineplot(data=training, x="X_test", y="Y_test", color="blue")
+sns.lineplot(x=grid, y=grid, color="red")
+sns.lineplot(data=predictions, x=grid, y=predicted, color="blue")
+sns.lineplot(data=predictions, x=np.ones(150) * 0.8, y=np.linspace(-0.1, 1.1, 150), color="black")
+plt.axvline(0.8, color='k', linestyle='--')
+ax.set_title("Loss: {}".format(loss))
+fig.legend(labels=['reference', 'predicted', 'cutoff'])
 plt.show()
